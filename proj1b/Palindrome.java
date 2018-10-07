1837a79311cd417368bb2114de61f5e14c514ee5
Loukas <m13685760596@gmail.com>
2018-10-07 14:33:53 -0400

    complete proj1b

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,8 +1,47 @@
 public class Palindrome {
 
     public Deque<Character> wordToDeque(String word) {
-        return null;
+        Deque<Character> d = new LinkedListDeque<>();
+        for(int i = 0; i < word.length(); i++) {
+            d.addLast(word.charAt(i));
+        }
+        return d;
     }
+
+    /** implementing is Palindrome function and its helper function */
+    public boolean isPalindrome(String word) {
+        return helper(wordToDeque(word));
+    }
+
+    public boolean helper(Deque<Character> deq) {
+        if(deq.size() == 0 || deq.size() == 1) return true;
+        char first = deq.removeFirst();
+        char last = deq.removeLast();
+
+        if(first == last) {
+            return helper(deq);
+        }
+        return false;
+    }
+
+    /** */
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        return helper1(wordToDeque(word), cc);
+    }
+
+    public boolean helper1(Deque<Character> deq, CharacterComparator cc) {
+        if(deq.size() == 0 || deq.size() == 1) return true;
+        char first = deq.removeFirst();
+        char last = deq.removeLast();
+
+        if(cc.equalChars(first, last)) {
+            return helper1(deq, cc);
+        }
+        return false;
+    }
+
+
 
 
 
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -15,4 +15,19 @@ public class TestPalindrome {
         }
         assertEquals("persiflage", actual);
     } // Uncomment this class once you've created your Palindrome class. */
+
+    @Test
+    public void testIsPalindrome() {
+        assertTrue(palindrome.isPalindrome("cac"));
+        assertTrue(palindrome.isPalindrome("caccac"));
+        assertTrue(palindrome.isPalindrome("fgf"));
+        assertFalse(palindrome.isPalindrome("asdfgfdsasd"));
+    }
+
+    static CharacterComparator offByOne = new OffByOne();
+    @Test
+    public void testIsPalindrome1() {
+        CharacterComparator cc = new OffByOne();
+        assertTrue(palindrome.isPalindrome("asb", cc));
+    }
 }
